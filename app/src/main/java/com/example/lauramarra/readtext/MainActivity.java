c9e3634a6e33574391c44ba21363ac0b4e7f55f2
lmpires <lauramarrap@gmail.com>
2015-02-06 00:18:02 -0200

    funcionando leitura

--- app/src/main/java/com/example/lauramarra/readtext/MainActivity.java
+++ app/src/main/java/com/example/lauramarra/readtext/MainActivity.java
@@ -1,23 +1,17 @@
 package com.example.lauramarra.readtext;
 
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    TextView view;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,42 +19,56 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView)findViewById(R.id.view);
-
-        String data = readTextFile(this);
-        textView.setText(data);
-
+        textView = (TextView) findViewById(R.id.view);
+        getFields();
     }
 
-    public static String readTextFile(Context ctx)
-    {
-        InputStream inputStream = null;
-        try {
-            inputStream = ctx.getAssets().open("doc.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void getFields() {
 
-        InputStreamReader inputreader = new InputStreamReader(inputStream);
-        BufferedReader bufferedreader = new BufferedReader(inputreader);
-        String line;
-        StringBuilder stringBuilder = new StringBuilder();
-        try
-        {
-            while (( line = bufferedreader.readLine()) != null)
-            {
-                stringBuilder.append(line);
-                stringBuilder.append('\n');
+        BufferedReader bufferedReader = null;
+        String codigo, nome, periodo, creditos, preRequisito_1, preRequisito_2, preRequisito_3, diaSemana;
+        //long e;
+        try {
+            String sCurrentLine = null;
+            bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("123.txt")));
+
+            while ((sCurrentLine = bufferedReader.readLine()) != null) {
+
+                //if (sCurrentLine != null);
+                String[] message = sCurrentLine.split(",");
+
+                codigo = message[0];
+                nome = message[1];
+                periodo = message[2];
+                creditos = message[3];
+                preRequisito_1 = message[4];
+                preRequisito_2 = message[5];
+                preRequisito_3 = message[6];
+                diaSemana = message[7];
+
+                textView.setText("Código: " + codigo + "\n" +
+                        "nome: " + nome + "\n" +
+                        "periodo: " + periodo + "\n" +
+                        "credito: : " + creditos + "\n" +
+                        "Pré requisito 1: " + preRequisito_1 + "\n" +
+                        "Pré requisito 2: " + preRequisito_2 + "\n" +
+                        "Pré requisito 3: " + preRequisito_3 + "\n" +
+                        "Dia da semana: " + diaSemana);
+
+                //e = db2.insertRecord(faculty1, deparment1, name1, officeNumber1, phone1, email1, email1);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) bufferedReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
         }
-        catch (IOException e)
-        {
-            return null;
-        }
-        return stringBuilder.toString();
     }
 
-    }
+}
 
 
 
